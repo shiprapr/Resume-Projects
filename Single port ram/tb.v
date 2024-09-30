d55e6b5f5958a5336c94e8f3a5c3618e1d14880f
shiprapr <84346339+shiprapr@users.noreply.github.com>
2024-09-30 19:31:15 +0530

    Add files via upload

---
+++ Single port ram/tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+
+
+
+module tb();
+reg clk,rst,en,wr_rd;
+reg [7:0] addr;
+reg [3:0] data_in;
+wire [3:0] data_out;
+
+single_port_ram uut(clk,rst,addr,data_in,data_out,wr_rd,en);
+
+always begin
+#5 clk=~clk;
+end
+initial begin
+clk=0;
+en=0;
+#15 en=0;
+#15 en=1;rst=0;data_in=4'b0001;addr=8'hAb;
+#15 data_in=4'b0010;addr=8'hBA;
+#40 rst=1;addr=8'hBA;
+#15 addr=8'hAB;
+#15
+
+$stop;
+
+end
+endmodule
